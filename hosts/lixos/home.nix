cdffb9fa6a0dbb180bf4b2515d02b7b19d141f5c
osmo1 <osmo@osmo.zip>
2024-06-26 16:25:15 +0300

    plasma configuration is hard

--- hosts/lixos/home.nix
+++ hosts/lixos/home.nix
@@ -1,11 +1,13 @@
-{ inputs, plasma-manager, ... }:
+{ inputs, ... }:
 {
     imports = [
     	inputs.home-manager.nixosModules.home-manager
-    	plasma-manager.homeManagerModules.plasma-manager
+	../../common/optional/plasma/rc2nix.nix
     ];
-    home-manager.users.osmo = {
+
+    home-manager.users.osmo = { inputs, ... }: {
+	imports = [ inputs.plasma-manager.homeManagerModules.plasma-manager ];
 	home.stateVersion = "24.05";
-    };
     programs.plasma.enable = true;
+    };
 }
