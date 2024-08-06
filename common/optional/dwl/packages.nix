42a5e7f5b03ca6a10c8393524f47457edf44b0f4
osmo1 <osmo@osmo.zip>
2024-08-06 17:53:11 +0300

    Gui to

---
+++ common/optional/dwl/packages.nix
@@ -0,0 +1,10 @@
+{ pkgs, unstable-pkgs, ... }: {
+        users.users.osmo.packages = with pkgs; [
+            wireguard-tools
+	    floorp
+	    bitwarden
+	    thunderbird
+	    capitaine-cursors
+	    alacritty
+        ];
+}
