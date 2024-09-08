e607b26a419ef367c68466ab64bf58cd2dd4541c
osmo1 <osmo@osmo.zip>
2024-09-08 16:35:55 +0300

    Working vpn to other server

--- common/optional/vpn.nix
+++ common/optional/vpn.nix
@@ -1,4 +1,4 @@
-{ config, ... }: {
+{ config, pkgs, lib, ... }: {
   sops.secrets."wireguard/oraakeli/private".path = "/persist/run/secrets/wireguard/oraakeli/private";
   sops.secrets."wireguard/oraakeli/preshare".path = "/persist/run/secrets/wireguard/oraakeli/preshare";
   networking.firewall = {
@@ -6,16 +6,17 @@
   };
    networking.wg-quick.interfaces = {
     wg0 = {
-      address = [ "10.2.0.2/32" ];
+      address = [ "10.13.13.2" ];
       listenPort = 51820; 
       privateKeyFile = config.sops.secrets."wireguard/oraakeli/private".path;
+      dns = [ "8.8.8.8" "10.13.13.1" ];
 
       peers = [
         {
           publicKey = "w4O5dZkMcHkoMci+DyYdsp21BdAQFq6rPj5dDy5FSSw=";
 	  presharedKeyFile = config.sops.secrets."wireguard/oraakeli/preshare".path;
 
-          allowedIPs = [ "0.0.0.0/0" ];
+          allowedIPs = [ "0.0.0.0/0" "::/0" ];
           endpoint = "158.179.207.29:51820"; 
 	  persistentKeepalive = 25;
         }
