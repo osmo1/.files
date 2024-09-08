11c3b5acdb0065ca969633314b046463471128f7
osmo1 <osmo@osmo.zip>
2024-09-08 16:35:41 +0300

    This needs to be changed though

---
+++ common/optional/vpn.nix
@@ -0,0 +1,25 @@
+{ config, ... }: {
+  sops.secrets."wireguard/oraakeli/private".path = "/persist/run/secrets/wireguard/oraakeli/private";
+  sops.secrets."wireguard/oraakeli/preshare".path = "/persist/run/secrets/wireguard/oraakeli/preshare";
+  networking.firewall = {
+    allowedUDPPorts = [ 51820 ];
+  };
+   networking.wg-quick.interfaces = {
+    wg0 = {
+      address = [ "10.2.0.2/32" ];
+      listenPort = 51820; 
+      privateKeyFile = config.sops.secrets."wireguard/oraakeli/private".path;
+
+      peers = [
+        {
+          publicKey = "w4O5dZkMcHkoMci+DyYdsp21BdAQFq6rPj5dDy5FSSw=";
+	  presharedKeyFile = config.sops.secrets."wireguard/oraakeli/preshare".path;
+
+          allowedIPs = [ "0.0.0.0/0" ];
+          endpoint = "158.179.207.29:51820"; 
+	  persistentKeepalive = 25;
+        }
+      ];
+    };
+  };
+}
