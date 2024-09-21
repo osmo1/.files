53f2159e6694366c11924612dc02efe6f66fa47d
osmo1 <osmo@osmo.zip>
2024-09-21 13:31:05 +0300

    WSL now actually wsl and testeri back to server conf

--- hosts/nix-wsl/default.nix
+++ hosts/nix-wsl/default.nix
@@ -4,11 +4,14 @@
     imports = (configLib.scanPaths ./.)
     ++ [
     	../../common/core
+	../../common/optional/cybersecurity.nix
     ];
 
     system.stateVersion = "24.05";
 
     networking.hostName = "nix-wsl";
+      wsl.enable = true;
+  wsl.defaultUser = "osmo";
 
     users.users = {
         osmo.openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAeBjrcy3Gh5gfHlFVNmAVSb2iTamDdOW4PNXH1pU2bQ osmo@osmo.zip"];
