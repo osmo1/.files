00b69ed122b85a5fcdc76d3c8f94b0d1360d0fa5
osmo1 <osmo@osmo.zip>
2024-11-13 17:57:44 +0200

    dwl additions
    still requires a lot of work

--- common/optional/dwl/default.nix
+++ common/optional/dwl/default.nix
@@ -1,21 +1,14 @@
 { configLib, pkgs, ... }:
 {
-  imports = (configLib.scanPaths ./.) ++ [ ../sound.nix ];
-  #home-manager.users.osmo = {}: {} 
+  imports = (configLib.scanPaths ./.) ++ [ ../desktop ];
+  environment.systemPackages =
+    (with pkgs.stable; [
+      dwl
+      somebar
+    ])
 
-  users.users.osmo.packages = with pkgs; [
-    # dwl
-    (pkgs.dwl.override {
-      # trying to supply config.home.homeDirectory here leads to "impure" usage.
-      # so disabling it for now.
-      # conf = (builtins.readFile "${config.home.homeDirectory}/.config/dwl/config.h");
-      conf = ./config.h;
-    })
-  ];
-  security.polkit.enable = true;
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-  };
+    ++
 
+    (with pkgs.unstable; [
+    ]);
 }
--- common/optional/dwl/packages.nix
+++ common/optional/dwl/packages.nix
@@ -1,11 +1,5 @@
 { pkgs, unstable-pkgs, ... }:
 {
   users.users.osmo.packages = with pkgs; [
-    wireguard-tools
-    floorp
-    bitwarden
-    thunderbird
-    capitaine-cursors
-    alacritty
   ];
 }
