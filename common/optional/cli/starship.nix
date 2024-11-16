104d35e14eda6eea040550796b3ba5e69511c21c
osmo1 <osmo@osmo.zip>
2024-11-16 13:12:48 +0200

    Starship

---
+++ common/optional/cli/starship.nix
@@ -0,0 +1,60 @@
+{
+  home-manager.users.osmo = { config, ... }: {
+      programs.starship = {
+        enable = true;
+        enableTransience = true;
+        enableZshIntegration = true;
+        settings = {
+
+        };
+         settings = {
+              add_newline = false;
+              format = "[](#7dcfff)$directory[](fg:#7aa2f7 bg:#7dcfff)$git_branch$git_status[](fg:#bb9af7 bg:#7aa2f7)$nodejs$rust$golang$php[](fg:#bb9af7)\n$character";
+              character = {
+                success_symbol = "[󰘧](#9ece6a)";
+                error_symbol = "[󰘧](#f7768e)";
+              };
+              directory = {
+                style = "fg:#1a1b26 bg:#7dcfff";
+                format = "[ $path ]($style)";
+                truncation_length = 3;
+                truncation_symbol = "…/";
+              };
+              directory.substitutions = {
+                "Documents" = "󰈙 ";
+                "Downloads" = " ";
+                "Music" = " ";
+                "Pictures" = " ";
+                "Videos" = " ";
+#"Projects" = " ";
+                "Games" = "󰖺 ";
+                "Misc" = "󰫢 ";
+              };
+              git_branch = {
+                symbol = "";
+                style = "fg:#1a1b26 bg:#7aa2f7";
+                format = "[ $symbol $branch ]($style)";
+              };
+              git_status = {
+                style = "fg:#1a1b26 bg:#7aa2f7";
+                format = "[($all_status$ahead_behind )]($style)";
+              };
+              rust = {
+                symbol = "";
+                style = "fg:#1a1b26 bg:#bb9af7";
+                format = "[ $symbol($version) ]($style)";
+              };
+              c = {
+                symbol = "";
+                style = "fg:#1a1b26 bg:#bb9af7";
+                format = "[ $symbol($version) ]($style)";
+              };
+              nodejs = {
+                symbol = "";
+                style = "fg:#1a1b26 bg:#bb9af7";
+                format = "[ $symbol($version) ]($style)";
+              };
+            };
+      };
+  };
+}
