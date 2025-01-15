c61f08a837e5a111df17cad9afe54a1c655b3e79
osmo1 <osmo@osmo.zip>
2025-01-15 22:03:26 +0200

    traefik update

--- modules/containers/traefik.nix
+++ modules/containers/traefik.nix
@@ -35,8 +35,16 @@ in
   };
 
   config = mkIf cfg.enable {
-    sops.secrets."containers/traefik-api" = { };
-    sops.secrets."containers/traefik-secret" = { };
+    sops.secrets."containers/traefik-api" = {
+        owner = "osmo";
+        group = "users";
+        mode = "700";
+    };
+    sops.secrets."containers/traefik-secret" = {
+        owner = "osmo";
+        group = "users";
+        mode = "700";
+    };
     virtualisation.oci-containers = {
       containers.traefik = {
         hostname = "traefik";
@@ -47,20 +55,27 @@ in
           "${toString cfg.uiPort}:8080"
         ];
         environment = {
-          PORKBUN_API_KEY_FILE = "/keys/api";
-          PORKBUN_SECRET_API_KEY_FILE = "/keys/secret";
+          PORKBUN_API_KEY_FILE = "/keys/traefik-api";
+          PORKBUN_SECRET_API_KEY_FILE = "/keys/traefik-secret";
         };
         volumes = [
-          "${cfg.dataLocation}:/letsencrypt"
-          "${cfg.dataLocation}/keys:/keys"
+          "${cfg.dataLocation}/config:/config"
+          "${cfg.dataLocation}/certs:/letsencrypt"
+          "/var/run/secrets/containers/traefik-api:/keys/traefik-api"
+          "/var/run/secrets/containers/traefik-secret:/keys/traefik-secret"
         ];
         cmd = [
           "--api.insecure=true"
           "--api.dashboard=true"
+          "--providers.file.directory=/config"
+          "--providers.file.watch=true"
           "--providers.docker=true"
           "--providers.docker.exposedbydefault=false"
           "--providers.docker.endpoint=tcp://${
-            if config.networking.hostName == "klusteri-0" then "192.168.11.10" else "192.168.11.11"
+            if config.networking.hostName == "klusteri-0" then "192.168.11.10"
+            else if config.networking.hostName == "klusteri-1" then "192.168.11.11"
+            else if config.networking.hostName == "klusteri-2" then "192.168.11.12"
+            else "192.168.11.200"
           }:2375" # Use the proxy instead of the direct socket
           "--entrypoints.web.address=:80"
           "--entrypoints.web.http.redirections.entrypoint.to=websecure"
@@ -82,7 +97,7 @@ in
                 "homepage.group" = "Network";
                 "homepage.name" = "Traefik";
                 "homepage.icon" = "traefik.png";
-                "homepage.href" = "https://${cfg.options.url}";
+                "homepage.href" = "https://traefik.${cfg.options.url}";
                 "homepage.description" = "Reverse proxies";
               }
             else
@@ -94,7 +109,7 @@ in
               if cfg.enableTraefik == true then
                 {
                   "traefik.enable" = "true";
-                  "traefik.http.routers.traefik.rule" = "Host(`${cfg.options.url}`)";
+                  "traefik.http.routers.traefik.rule" = "Host(`traefik.${cfg.options.url}`)";
                   "traefik.http.routers.traefik.entrypoints" = "websecure";
                   "traefik.http.routers.traefik.tls.certresolver" = "porkbun";
                   "traefik.http.services.traefik.loadbalancer.server.port" = "8080";
@@ -115,6 +130,8 @@ in
     systemd.tmpfiles.rules = [
       "d ${cfg.dataLocation} 0770 osmo users - -"
       "d ${cfg.dataLocation}/keys 0770 osmo users - -"
+      "d ${cfg.dataLocation}/certs 0770 osmo users - -"
+      "d ${cfg.dataLocation}/config 0770 osmo users - -"
     ];
     #systemd.services.podman-traefik.serviceConfig.User = "osmo";
   };
