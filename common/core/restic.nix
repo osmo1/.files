6ae12de37204732b31f950d68120e3ba12b94d1b
osmo1 <osmo@osmo.zip>
2025-02-13 20:34:11 +0200

    Restic module and .secrets git-agecrypt fix

---
+++ common/core/restic.nix
@@ -0,0 +1,90 @@
+{ lib, config, pkgs, ... }:
+with lib;
+let
+  cfg = config.borgus;
+in
+{
+  options.borgus = {
+    enable = mkEnableOption "Enable borgmatic backups";
+    extraPatterns = mkOption {
+        type = types.listOf types.str;
+        default = [];
+#description = "Extra patterns for the home directory";
+    };
+    extraBackups = mkOption {
+        type = types.attrs;
+        default = {};
+#description = "Extra backup configurations";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    sops.secrets = {
+        borg-age = {
+            owner = "osmo";
+            group = "users";
+            mode = "600";
+        };
+        "nixos/borgus/ssh/private" = {
+            path = "/home/osmo/.ssh/borgus";
+            owner = "osmo";
+            group = "users";
+            mode = "600";
+        };
+    };
+    services.restic.backups = {
+          main-backup = {
+            paths = [ "/home/osmo" ];
+            user = "osmo";
+            exclude = [
+              "/home/osmo/games"
+              "/home/osmo/Games"
+              "/home/osmo/.cache"
+              "/home/osmo/.steam"
+              "/home/osmo/.local/share/Steam"
+            ];
+            initialize = true;
+            passwordFile = config.sops.secrets.borg-age.path;
+            repository = "sftp:restic@192.168.11.12:/backups/${config.networking.hostName}-home";
+            extraOptions = [ "sftp.command='ssh restic@192.168.11.12 -i /home/osmo/.ssh/borgus -s sftp'" ];
+            extraBackupArgs = [
+                "--compression auto"
+                "--read-concurrency 20"
+            ];
+            timerConfig = {
+              OnCalendar = "19:00";
+              Persistent = true;
+            };
+#inhibitsSleep = true;
+            pruneOpts = [
+                "--keep-last 3"
+                "--keep-daily 7"
+                "--keep-weekly 4"
+                "--keep-monthly 1"
+            ];
+          };
+        };
+      systemd.services.restic-backups-main-backup.unitConfig.OnFailure = "notify-backup-failed.service";
+
+      systemd.services."notify-backup-failed" = {
+        enable = true;
+        description = "Notify on failed backup";
+        serviceConfig = {
+          Type = "oneshot";
+          User = config.users.users.osmo.name;
+        };
+
+        # required for notify-send
+        environment.DBUS_SESSION_BUS_ADDRESS = "unix:path=/run/user/${
+                toString config.users.users.osmo.uid
+              }/bus";
+
+        script = ''
+          ${pkgs.libnotify}/bin/notify-send --urgency=critical \
+            "Backup failed" \
+            "$(journalctl -u restic-backups-daily -n 5 -o cat)"
+        '';
+      };
+      environment.systemPackages = [ pkgs.restic ];
+    };
+}
