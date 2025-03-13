cfec3581370dcbe88e76e4b8b5c3592b405a6f6b
osmo1 <osmo@osmo.zip>
2025-03-13 21:00:38 +0200

    Fastfetch conf
    Pretty much

--- common/optional/cli/fastfetch.nix
+++ common/optional/cli/fastfetch.nix
@@ -1,30 +1,41 @@
+{ config, ... }:
+let
+  theme = (if config.hostSpec.theme == "Tokyo Night" then "Tokyo Night" else "Kanagawa");
+in
 {
-  home-manager.users.osmo =
-    { config, ... }:
-    {
-      fastfetch = {
-        enable = true;
-        settings = {
-          modules = [
-            "os"
-            "host"
-            "display"
-            "kernel"
-            "uptime"
-            "packages"
-            "shell"
-            "de"
-            {
-              type = "theme";
-              key = (if config.hostSpec == "Tokyo Night" then "Tokyo Night" else "Kanagawa");
-            }
-            "terminal"
-            "cpu"
-            "gpu"
-            "memory"
-            "disk"
-          ];
-        };
+  home-manager.users.osmo = {
+    programs.fastfetch = {
+      enable = true;
+      settings = {
+        modules = [
+          "os"
+          "host"
+          "kernel"
+          "uptime"
+          "packages"
+          "shell"
+          "de"
+          {
+            type = "theme";
+            format = theme;
+          }
+          "terminal"
+          "cpu"
+          "gpu"
+          "display"
+          "memory"
+          "disk"
+          "colors"
+          {
+            type = "custom";
+            format = "";
+          }
+          {
+            type = "custom";
+            format = "";
+          }
+        ];
       };
     };
+  };
 }
