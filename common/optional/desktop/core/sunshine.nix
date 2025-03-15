d14aa45b44a3341521c120e9f3aadb156bb9c08f
osmo1 <osmo@osmo.zip>
2025-03-15 11:48:34 +0200

    Sunshine manual ports
    Dunno what broke but this seems to have fixed it

--- common/optional/desktop/core/sunshine.nix
+++ common/optional/desktop/core/sunshine.nix
@@ -5,4 +5,22 @@
     capSysAdmin = true;
     openFirewall = true;
   };
+  networking.firewall = {
+    allowedTCPPorts = [
+      47984
+      47989
+      47990
+      48010
+    ];
+    allowedUDPPortRanges = [
+      {
+        from = 47998;
+        to = 48000;
+      }
+      {
+        from = 8000;
+        to = 8010;
+      }
+    ];
+  };
 }
