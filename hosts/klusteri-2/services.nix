c01f281cc2081d5a20194500ed9f457a717b66a4
osmo1 <osmo@osmo.zip>
2025-08-02 23:40:48 +0300

    Copyparty

--- hosts/klusteri-2/services.nix
+++ hosts/klusteri-2/services.nix
@@ -4,5 +4,29 @@
   ];
 
   services.containers = {
+    dockerproxy.enable = true;
+    copyparty = {
+      enable = true;
+      version = "1.18.9";
+      ports.ui = 80;
+      volumes = {
+        config = "/home/osmo/copyparty";
+        data = "/home/osmo/media";
+      };
+      enableHomePage = true;
+      traefik = {
+        enable = true;
+        urlBase = "klusteri-2.kotiserweri.zip";
+      };
+    };
+    traefik = {
+      enable = true;
+      version = "v3.4.3";
+      uiPort = 180;
+      dataLocation = "/home/osmo/traefik";
+      options = {
+        url = "klusteri-2.kotiserweri.zip";
+      };
+    };
   };
 }
