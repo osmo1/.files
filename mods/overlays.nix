369a2cf481dc3c1eb05be6c7207fa6041a458fbd
osmo1 <osmo@osmo.zip>
2025-11-04 18:34:24 +0200

    Beszle update

--- mods/overlays.nix
+++ mods/overlays.nix
@@ -75,9 +75,31 @@ let
                   rev = "523f7004dce885fe38b4f80e34a8f76dc8ea98b5";
                   hash = "sha256-U8SK1n1fLjYqlzAH2eU4MLBIM+QMAt35sEbY9EVGrfQ=";
                 };
-
               }
             );
+            beszel =
+              let
+                version = "0.15.3";
+              in
+              (final.beszel.override {
+                buildGoModule = final.buildGoModule.override { go = final.go_1_25; };
+              }).overrideAttrs
+                {
+                  version = version;
+                  src = final.fetchFromGitHub {
+                    owner = "henrygd";
+                    repo = "beszel";
+                    tag = "v${version}";
+                    hash = "sha256-BG457HIwbk+axZ4opTU9bSWq6YLNM8L/GVe2eQOab/s=";
+                  };
+                  sourceRoot = "";
+                  vendorHash = "sha256-MP06kn8iFpz9AOv3LQqSGGSe4c7JvP/Kp4Nmsuga99o=";
+                  subPackages = [ "internal/cmd/agent" ];
+                  postInstall = ''
+                    mkdir -p $out/bin
+                    mv $out/bin/agent $out/bin/beszel-agent
+                  '';
+                };
           })
         ];
     };
